107a79eb01a975b49efe646cb14c17c15d2af72c
Animeshka39 <82146267+Animeshka39@users.noreply.github.com>
2021-12-24 01:36:03 +0300

    Add files via upload

---
+++ PokerSimulator-master/Poker/DeckOfCards.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poker
+{
+    class DeckOfCards : Card
+    {
+        const int NumberOfCards = 52; //Загальна кількість карт у колоді
+        private Card[] Deck; //Масив усіх карт
+
+        public DeckOfCards()
+        {
+            Deck = new Card[NumberOfCards];
+        }
+
+        public Card[] GetDeck { get { return Deck; } } //Отримати поточну колоду
+
+        //Створіть колоду з 52 карт, по 13 масті, 4 масті
+        public void SetupDeck()
+        {
+            int i = 0;
+            foreach(Suit s in Enum.GetValues(typeof(Suit)))
+            {
+                foreach(Value v in Enum.GetValues(typeof(Value)))
+                {
+                    Deck[i] = new Card { mySuit = s, myValue = v };
+                    i++;
+                }
+            }
+            ShuffleCards();
+        }
+        // Перетасуйте колоду
+        public void ShuffleCards()
+        {
+            Random rand = new Random();
+            Card temp;
+            //Виконайте перемішування 1000 разів
+            for (int shuffle = 0; shuffle < 1000; shuffle++)
+            {
+                for (int i = 0; i < NumberOfCards; i++)
+                {
+                    //Поміняйте карти
+                    int SecondCardIndex = rand.Next(13);
+                    temp = Deck[i];
+                    Deck[i] = Deck[SecondCardIndex];
+                    Deck[SecondCardIndex] = temp;
+                }
+            }
+        }
+    }
+}
---
+++ PokerSimulator-master/Poker/DrawCards.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poker
+{
+    class DrawCards
+    {
+        //Намалюйте контури карток
+        public static void DrawCardOutline(int xCoor, int yCoor)
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+
+            int x = xCoor * 12; //10 символів підкреслення для малювання лінії та один пробіл з обох сторін для країв
+            int y = yCoor;
+
+
+            Console.SetCursorPosition(x, y);//Початкове положення, де брати картку
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write(" __________\n");//Верхній край картки
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.SetCursorPosition(x, y + 1 + i);
+
+                if (i != 9)
+                {
+                    Console.WriteLine("|          |");//Лівий і правий краї картки
+                }
+                else
+                {
+                    Console.WriteLine("|__________|");//Нижня частина картки
+                }
+            }
+        }
+        //Display suit and value on the card
+        public static void DrawCardSuitValue(Card card, int xCoor, int yCoor)
+        {
+            char cardSuit = ' ';
+            int x = xCoor * 12;
+            int y = yCoor;
+
+            //Кодуйте кожен масив байтів за допомогою CodePage437 у символ для символу масті
+            //Серця і ромби червоні, піки і трефи чорні
+            switch (card.mySuit)
+            {
+                case Card.Suit.Hearts:
+                    cardSuit = Encoding.GetEncoding(437).GetChars(new byte[] { 3 })[0];
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case Card.Suit.Diamonds:
+                    cardSuit = Encoding.GetEncoding(437).GetChars(new byte[] { 4 })[0];
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case Card.Suit.Clubs:
+                    cardSuit = Encoding.GetEncoding(437).GetChars(new byte[] { 5 })[0];
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    break;
+                case Card.Suit.Spades:
+                    cardSuit = Encoding.GetEncoding(437).GetChars(new byte[] { 6 })[0];
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    break;
+            }
+
+            //Відображати закодований символ на всіх 4 кутах і значення
+            Console.SetCursorPosition(x + 2, y + 2);
+            Console.Write(cardSuit);
+            Console.SetCursorPosition(x + 9, y + 2);
+            Console.Write(cardSuit);
+            Console.SetCursorPosition(x + 2, y + 9);
+            Console.Write(cardSuit);
+            Console.SetCursorPosition(x + 9, y + 9);
+            Console.Write(cardSuit);
+            Console.SetCursorPosition(x + 4, y + 5);
+            Console.Write(card.myValue);
+            Console.ForegroundColor = ConsoleColor.Black;
+
+        }
+    }
+}
+
